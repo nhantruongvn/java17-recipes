2cd18da0d221a0ca26342f784a7d20353dc0d61e
nhantruongvn <108405814+nhantruongvn@users.noreply.github.com>
2022-08-16 22:06:58 +0700

    add Chapter1 HelloWorld

---
+++ src/org/java17recipes/chapter01/recipe01_05/HelloMessage.java
@@ -0,0 +1,15 @@
+package org.java17recipes.chapter01.recipe01_05;
+
+public class HelloMessage {
+    private String message = "";
+    public HelloMessage() {
+        this.message = "Default Message";
+    }
+    public void setMessage (String m) {
+        this.message = m;
+    }
+    public String getMessage () {
+        //it changes the message to uppercase
+        return message.toUpperCase();
+    }
+}
