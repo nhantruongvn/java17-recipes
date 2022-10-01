00cd00c00f0b8091f7974ba4c576927c713c6694
nhantruongvn <108405814+nhantruongvn@users.noreply.github.com>
2022-10-01 17:54:10 +0700

    finish chapter 1

---
+++ src/org/java17recipes/chapter01/recipe01_12/JavadocExample.java
@@ -0,0 +1,30 @@
+package org.java17recipes.chapter01.recipe01_12;
+
+import java.math.BigInteger;
+
+// In Command Prompt run: javadoc JavadocExample.java -d JavaDoc
+
+public class JavadocExample {
+    /**
+     * Accepts an unlimited number of values and
+     * returns the sum.
+     *
+     * @param nums Must be an array of BigInteger values.
+     * @return Sum of all numbers in the array.
+     */
+    public static BigInteger addNumbers(BigInteger[] nums) {
+        BigInteger result = new BigInteger("0");
+        for (BigInteger num:nums){
+            result = result.add(num);
+        }
+        return result;
+    }
+    /**
+     * Test the addNumbers method.
+     * @param args not used
+     */
+    public static void main (String[] args) {
+        BigInteger[] someValues = {BigInteger.TEN, BigInteger.ONE};
+        System.out.println(addNumbers(someValues));
+    }
+}
