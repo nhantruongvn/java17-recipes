b87704551abf672f64617cb3fe21d93d657d1b95
nhantruongvn <108405814+nhantruongvn@users.noreply.github.com>
2022-10-22 23:51:26 +0700

    remove unneeded libraries

--- src/org/java17recipes/chapter03/recipe03_01/ApacheCommonsTextDemo.java
+++ src/org/java17recipes/chapter03/recipe03_01/ApacheCommonsTextDemo.java
@@ -5,7 +5,7 @@ import org.apache.commons.text.diff.EditScript;
 import org.apache.commons.text.diff.StringsComparator;
 import org.apache.commons.text.similarity.LongestCommonSubsequence;
 import org.apache.commons.text.translate.UnicodeEscaper;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ApacheCommonsTextDemo {
 
     @Test
-    @org.junit.jupiter.api.Test
     public void whenContainsWords_thenCorrect() {
         boolean containsWords = WordUtils
                 .containsAllWords("String to search", "to", "search");
@@ -21,16 +20,13 @@ class ApacheCommonsTextDemo {
         assertTrue(containsWords);
     }
     @Test
-    @org.junit.jupiter.api.Test
     public void whenCapitalized_thenCorrect() {
         String toBeCapitalized = "to be capitalized!";
         String result = WordUtils.capitalize(toBeCapitalized);
 
         assertEquals("To Be Capitalized!", result);
     }
-
-    @org.junit.jupiter.api.Test
-    @Test
+@Test
     public void whenEditScript_thenCorrect() {
         StringsComparator cmp = new StringsComparator("ABCFGH", "BCDEFG");
         EditScript<Character> script = cmp.getScript();
@@ -39,7 +35,6 @@ class ApacheCommonsTextDemo {
         assertEquals(4, mod);
     }
 
-    @org.junit.jupiter.api.Test
     @Test
     public void whenCompare_thenCorrect() {
         LongestCommonSubsequence lcs = new LongestCommonSubsequence();
@@ -48,7 +43,6 @@ class ApacheCommonsTextDemo {
         assertEquals(5, countLcs);
     }
 
-    @org.junit.jupiter.api.Test
     @Test
     public void whenTranslate_thenCorrect() {
         UnicodeEscaper ue = UnicodeEscaper.above(0);
